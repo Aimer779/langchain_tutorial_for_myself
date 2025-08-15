f2d59db0d37e9644324eddba0b2ae05a92c2a661
Aimer779 <2302500712@qq.com>
2025-08-15 19:04:38 +0800

    Add .gitignore and initial implementation of chat model using DeepSeek

---
+++ basics/components/chatmodels/ds_chat_test.py
@@ -0,0 +1,28 @@
+import getpass
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if not os.environ.get("DEEPSEEK_API_KEY"):
+    os.environ["DEEPSEEK_API_KEY"] = getpass.getpass("Enter API key for DeepSeek: ")
+
+from langchain_deepseek import ChatDeepSeek
+
+llm = ChatDeepSeek(
+    model="deepseek-chat",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+ai_msg = llm.invoke(messages)
+print(ai_msg.content)
